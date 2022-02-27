6f44ddc9b30e52e0f1377a8850d2261f20f48eef
Aldhair Vera Camacho <aldhairvera15@gmail.com>
2022-02-27 16:27:15 -0500

    fix

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ server.use(express.json());
 
 server.get('/',(req,res)=>{
   res.status(200,{'Content-Type':'text/json'});
-  console.log(req.headers);
-  res.send(`<h1>${req.headers['sec-ch-ua']}</h1>`)
+  const value=req.headers['sec-ch-ua']||'mi-super-navegador';
+  console.log(value);
+  res.send(`<h1>${value}</h1>`)
 });
 
 server.listen(PORT,()=>console.log(`Listening on port ${PORT}`))
