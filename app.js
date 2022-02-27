0c4dbaeceba28d4e6154aabef906cd844a3612d3
Aldhair Vera Camacho <aldhairvera15@gmail.com>
2022-02-27 16:23:51 -0500

    fix: exercise 7 - Header content

--- app.js
+++ app.js
@@ -11,11 +11,9 @@ server.use(express.urlencoded());
 server.use(express.json());
 
 server.get('/',(req,res)=>{
-  res.render('index')
+  res.status(200,{'Content-Type':'text/json'});
+  console.log(req.headers);
+  res.send(`<h1>${req.headers['sec-ch-ua']}</h1>`)
 });
-
-server.post('/',(req,res)=>{
-  res.send(`<h1>Hola ${req.body.name}!</h1>`);  
-})
 
 server.listen(PORT,()=>console.log(`Listening on port ${PORT}`))
